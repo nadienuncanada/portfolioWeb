723eab745638ef9ac25f4fd721185cfa4612dc0d
Ignacio <meloignacionicolas@gmail.com>
2025-02-06 19:29:18 -0300

    ADD: Courses

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -26,6 +26,13 @@ function HomePage() {
 							</li>
 							<li>
 								<a
+									href='#courses'
+									className='hover:text-blue-200'>
+									Cursos
+								</a>
+							</li>
+							<li>
+								<a
 									href='#contact'
 									className='hover:text-blue-200'>
 									Contacto
@@ -163,94 +170,135 @@ function HomePage() {
 					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
 						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
 							<div className='flex items-center'>
-								<span className='material-icons text-2xl mr-2'>calculate</span>
+								<span className='material-icons text-2xl mr-2'>html</span>
 								<h3 className='text-xl font-bold'>
-									Automatización de Facturación para Estudio Contable
+									Argentina Programa 4.0 – Introducción a Lógica de Programación
 								</h3>
 							</div>
-							<p>
-								Esta es un pequeño programa para un estudio contable que
-								necesitaba procesar archivos y subir ciertos datos de los mismos
-								a una hoja de google spreadsheets.
-							</p>
 							<div className='flex space-x-2 mt-4'>
 								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded hover:bg-gray-300'>
-									Python
+									Ruby
 								</span>
 								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									Selenium
+									Java
 								</span>
 								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									Google Spreadsheets API
+									HTML
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									CSS
 								</span>
 							</div>
 							<a
-								href='https://github.com/nadienuncanada/FacturacionCalculator'
+								href='https://www.linkedin.com/in/ignacio-melo-b2297a265/details/education/'
 								target='_blank'
 								rel='noopener noreferrer'
-								className='text-blue-600 hover:underline mt-4'>
-								Ver código
+								className='text-blue-600 hover:underline'>
+								Ver cerificado
+							</a>
+						</div>
+						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
+							<div className='flex items-center'>
+								<span className='material-icons text-2xl mr-2'>css</span>
+								<h3 className='text-xl font-bold'>
+									Argentina Programa 4.0 – Primeros pasos desarrollo frontend
+								</h3>
+							</div>
+							<div className='flex space-x-2 mt-4'>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									HTML
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									CSS
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									JavaScript
+								</span>
+							</div>
+							<a
+								href='https://www.linkedin.com/in/ignacio-melo-b2297a265/details/education/'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-blue-600 hover:underline'>
+								Ver cerificado
 							</a>
 						</div>
 						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
 							<div className='flex items-center'>
 								<span className='material-icons text-2xl mr-2'>
-									construction
+									insert_chart
 								</span>
-								<h3 className='text-xl font-bold'>Pagina Web Ferreteria</h3>
+								<h3 className='text-xl font-bold'>
+									Argentina Programa 4.0 – Procesamiento de datos con Python
+								</h3>
 							</div>
-							<p>
-								Proyecto realizado en la materia Ingenieria de Software,
-								aplicacion web creada para gestionar intercambio de productos de
-								ferreteria y compras dentro de la misma app con la API de
-								Mercado Pago{' '}
-							</p>
-							<div className='flex space-x-2 mt-4'>
-								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									React
-								</span>
-								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									Node.js
-								</span>
-								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									Express.js
-								</span>
-							</div>
-							<a
-								href='https://github.com/ImportFromCBastian/fedeteria-front'
-								target='_blank'
-								rel='noopener noreferrer'
-								className='text-blue-600 hover:underline'>
-								Ver código
-							</a>
-						</div>
-						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
-							<div className='flex items-center'>
-								<span className='material-icons text-2xl mr-2'>analytics</span>
-								<h3 className='text-xl font-bold'>Ejercitacion en BigData</h3>
-							</div>
-							<p>
-								Ejercicios y Trabajos Practicos de la materia Conceptos y
-								Aplicaciones en BigData, en donde se utilizaron herramientas
-								como Spark, SparkStreaming y Hadoop MapReduce.
-							</p>
 							<div className='flex space-x-2 mt-4'>
 								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
 									Python
 								</span>
 								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
-									Spark
+									Pandas
 								</span>
-								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded hover:bg-gray-300'>
-									Hadoop MapReduce
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									Machine Learning
 								</span>
 							</div>
 							<a
-								href='https://github.com/nadienuncanada/BigData'
+								href='https://www.linkedin.com/in/ignacio-melo-b2297a265/details/education/'
 								target='_blank'
 								rel='noopener noreferrer'
 								className='text-blue-600 hover:underline'>
-								Ver código
+								Ver cerificado
+							</a>
+						</div>
+						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
+							<div className='flex items-center'>
+								<span className='material-icons text-2xl mr-2'>bar_chart</span>
+								<h3 className='text-xl font-bold'>
+									Santander Open Academy – BigData
+								</h3>
+							</div>
+							<div className='flex space-x-2 mt-4'>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									Python
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									Data Analytics
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									Pandas
+								</span>
+							</div>
+							<a
+								href='https://www.linkedin.com/in/ignacio-melo-b2297a265/details/education/'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-blue-600 hover:underline'>
+								Ver cerificado
+							</a>
+						</div>
+						<div className='bg-white p-6 rounded-lg shadow-md flex flex-col items-center'>
+							<div className='flex items-center'>
+								<span className='material-icons text-2xl mr-2'>javascript</span>
+								<h3 className='text-xl font-bold'>Alura Oracle One</h3>
+							</div>
+							<div className='flex space-x-2 mt-4'>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									HTML
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									CSS
+								</span>
+								<span className='bg-gray-200 text-gray-800 text-sm font-semibold px-2 py-1 rounded  hover:bg-gray-300'>
+									JavaScript
+								</span>
+							</div>
+							<a
+								href='https://www.linkedin.com/in/ignacio-melo-b2297a265/details/education/'
+								target='_blank'
+								rel='noopener noreferrer'
+								className='text-blue-600 hover:underline'>
+								Ver cerificado
 							</a>
 						</div>
 					</div>
